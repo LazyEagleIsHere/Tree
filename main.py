ecefe06796994452214826efaef1c9f1ce1d6c21
LAzyEagleIsHere <lymivan1110@hotmail.com>
2026-05-07 22:54:06 +0800

    e

--- main.py
+++ main.py
@@ -60,10 +60,10 @@ def tutorial_screen():
     
 
 
-def show_text_on_screen(text, font_size, y_position):
+def show_text_on_screen(text, font_size, y_position, color=gray, x=0):
   font_local = pygame.font.Font(None, font_size)
-  text_render = font_local.render(text, True, gray)
-  text_rect = text_render.get_rect(center=(width // 2, y_position))
+  text_render = font_local.render(text, True, color)
+  text_rect = text_render.get_rect(center=(width // 2 + x, y_position))
   screen.blit(text_render, text_rect)
 
 def draw(text, font_size, y_position):
@@ -74,18 +74,68 @@ def draw(text, font_size, y_position):
 
 def start_screen():
   start = True
+  
+  btn_motion_y = 0
+  btn_motion_dir = -1
+  btn_max_motion = 3.5
+
+  text_colour_change_interval = 2
+  title_color = 150
+  mission_color = 100
+  
+  reverse_title_color_dir = 1
+  reverse_mission_color_dir = 1
+  
+  title_motion_x = 0
+  mission_motion_x = 0
+  title_motion_dir = -1
+  title_max_motion = 5
+
   while start:
     screen.fill(black)
-    show_text_on_screen("Bouncing Ball Game", 100, height // 4)
+    
+    btn_motion_y += 0.1 * btn_motion_dir
+    
+    if (btn_motion_y > btn_max_motion): 
+      btn_motion_dir = -1
+    elif (btn_motion_y < -btn_max_motion):
+      btn_motion_dir = 1
+    
+    
+    title_motion_x += 0.1 * title_motion_dir
+    mission_motion_x += 0.1 * -title_motion_dir
+    
+    if (title_motion_x > title_max_motion):
+      title_motion_dir = -1
+    elif (title_motion_x < -title_max_motion):
+      title_motion_dir = 1
+    
+    title_color += 0.75 * reverse_title_color_dir
+    mission_color += 1 * reverse_mission_color_dir
+    
+    # while ((title_color % 255) < 10 or (title_color * 2) % 255 < 10 or (title_color * 3) % 255 < 10):
+    #   title_color += 100
+    
+    if (title_color <= 100): 
+      reverse_title_color_dir = 1
+    elif (title_color * 1.5 >= 255):
+      reverse_title_color_dir = -1
+    
+    if (mission_color >= 255):
+      reverse_mission_color_dir = -1
+    elif (mission_color <= 100):
+      reverse_mission_color_dir = 1
+    
+    show_text_on_screen("Chop Down Elvin The Tree", 100, height // 4, (title_color, title_color * 1.5, title_color * 1.25), title_motion_x)
     # show_text_on_screen("Press spacebar to start...", 50, height // 2)
     
-    show_text_on_screen("Mission: Try your best to aim for as high a score as possible", 85, height // 2 - 100)
+    show_text_on_screen("Mission: Try your best to aim for as high a score as possible", 85, height // 2 - 100, (mission_color, mission_color, mission_color), mission_motion_x)
     
     mouse_pos = pygame.mouse.get_pos()
     
     pygame.mouse.set_visible(1)
     
-    start_btn = pygame.Rect(width // 2 - 200, height // 2 + 50, 400, 100)
+    start_btn = pygame.Rect(width // 2 - 200, height // 2 + 50 + btn_motion_y, 400, 100)
     
     if start_btn.collidepoint(mouse_pos):
       write(screen, start_btn, "Start", 65, "black", "gray69", 10)
@@ -94,7 +144,7 @@ def start_screen():
     
     # show_text_on_screen("Move the platform with arrow keys...", 65, height // 1.5)
     
-    tutorial = pygame.Rect(width // 2 - 200, height // 2 + 200, 400, 100)
+    tutorial = pygame.Rect(width // 2 - 200, height // 2 + 200 + btn_motion_y, 400, 100)
     
     if tutorial.collidepoint(mouse_pos):
       write(screen, tutorial, "Tutorial", 65, "black", "gray69", 10)
@@ -186,6 +236,9 @@ def main():
   game_running = True
   while game_running:
     screen.fill(black)
+    
+    
+    
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         pygame.quit()
